156ebb71abdb612f08e5ebc33e3c1cc504bfa9b2
YAVUZ <yylmz0510@gmail.com>
2023-09-18 20:43:52 +0200

    Test1 Updated ve Git Menu Uploaded

---
+++ src/GitMenu1.java
@@ -0,0 +1,3 @@
+public class GitMenu1 {
+
+}
--- src/Test1.java
+++ src/Test1.java
@@ -3,5 +3,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Test1");
         System.out.println("ADD LOCALLY!!!");
+        System.out.println("ADDED LOCALLY, pushed with GIT MENU!!!");
     }
 }
